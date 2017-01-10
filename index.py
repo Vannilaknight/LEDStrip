0672c1b08f13552dac76dc722774828d88102a40
Taylor Boyd <taylorboyd@Taytays-Mac.local>
2017-01-10 16:51:37 -0700

    I have no idea what im doing

--- index.py
+++ index.py
@@ -5,6 +5,12 @@ RED = 17
 GREEN = 22
 BLUE = 24
 
+totalTime = 10
+currentTime = 0
+
+color1 = [255,0,204]
+color2 = [51,204,205]
+
 pi = pigpio.pi()
 
 pi.set_PWM_dutycycle(RED, 0)
@@ -19,13 +25,22 @@ def LightDown(color, num):
         pi.set_PWM_dutycycle(color, num)
         LightDown(color, num - 1)
 
-def LightUp(color, num):
+def LightUp(num):
     if num == 128:
-        LightDown(color, num)
+        return
     else:
-        time.sleep(.1)
-        pi.set_PWM_dutycycle(color, num)
-        LightUp(color, num + 1)
+        num += .1
+        time.sleep(num)
+        interpolate(color1, color2, num/totalTime)
+        LightUp(num)
 
-LightUp(RED, 0)
+def interpolate(color1, color2, t):
+    r = color1[0] + (color2[0]- color1[0]) * t
+    g = color1[1]+ (color2[1]- color1[1]) * t
+    b = color1[2] + (color2[2]- color1[2]) * t
+    pi.set_PWM_dutycycle(RED, r)
+    pi.set_PWM_dutycycle(GREEN, g)
+    pi.set_PWM_dutycycle(BLUE, b)
+
+LightUp(0)
 pi.stop()
